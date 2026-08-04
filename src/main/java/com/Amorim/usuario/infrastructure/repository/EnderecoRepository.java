9f6d5aa4413b97d4e7707f16b7427716fee9a56a
Eduardo <eduardoamorim0005@gmail.com>
2026-08-04 01:11:21 -0300

    Adicionados classes de Security

---
+++ src/main/java/com/Amorim/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.Amorim.usuario.infrastructure.repository;
+
+
+import com.Amorim.usuario.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
---
+++ src/main/java/com/Amorim/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.Amorim.usuario.infrastructure.repository;
+
+
+import com.Amorim.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
